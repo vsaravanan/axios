611360e6fb164e4982608bf6553bd78b5d6ca43a
V Saravanan <saravanan.resume@gmail.com>
2018-06-17 19:09:37 +0800

    props

--- src/index.js
+++ src/index.js
@@ -2,41 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import axios from 'axios';
 
-import API from './api';
-
-export default class PersonList extends React.Component {
-  state = {
-    id: '',
-  }
-
-  handleChange = event => {
-    this.setState({ id: event.target.value });
-  }
-
-  handleSubmit = async event => {
-    event.preventDefault();
-  
-    // Promise is resolved and value is inside of the response const.
-    const response = await API.delete(`users/${this.state.id}`);
-  
-    console.log(response);
-    console.log(response.data);
-  };
-
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person ID:
-            <input type="text" name="id" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Delete</button>
-        </form>
-      </div>
-    )
-  }
+ 
+class MyComponent extends React.Component{
+        render(){
+        return(   
+              <div>
+               <h1>Hello</h1>
+               <Header name="maxx" id="101"/>
+           </div>
+        );
+    }
 }
-
-ReactDOM.render(<PersonList />, document.getElementById('root'));
+ 
+function Header(props) {
+    return (              
+           <div>
+            <Footer name = {props.name} id = {props.id}/>
+           </div>
+    );
+}
+function Footer(props) {
+    return (             
+           <div>             
+            <h1> Welcome : {props.name}</h1>             
+            <h1> Id is : {props.id}</h1>
+           </div>
+  );
+}
+ReactDOM.render(
+   <MyComponent/>, document.getElementById('root')
+);
