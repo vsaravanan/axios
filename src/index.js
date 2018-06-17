3ff2e7e7e2827fc30cda60522b1d5cf532263f86
V Saravanan <saravanan.resume@gmail.com>
2018-06-17 19:19:56 +0800

    Router

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import axios from 'axios';
+import {
+  BrowserRouter,
+  Route,
+  Link
+  } from 'react-router-dom';
 
-class MyComponent extends React.Component {
-  constructor() {
-      super();
-      this.state = {
-          name: 'Maxx',
-          id: '101'
-      }
-  }
-  render()
-      {
-          setTimeout(()=>{this.setState({name:'Jaeha', id:'222'})},2000)
-          return (             
-                 <div>                     
-                   <h1>Hello {this.state.name}</h1>
-                   <h2>Your Id is {this.state.id}</h2>
-                 </div>
-           );
-      }
-  }
+const About = () => (
+  <h1>About.....</h1>
+);
+
+const Topics = () => (
+  <h1>Topics.....</h1>
+);
+
+const MyComponent= () => (
+    <BrowserRouter>
+    <div>
+    <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/topics">Topics</Link></li>
+    </ul>
+    <Route exact path="/" render={ ( ) => (<h2> HomePage </h2>) } />
+    <Route path="/about" component={About}/>
+    <Route path="/topics" component={Topics}/>
+    </div>
+    </BrowserRouter>
+);
+
 
 ReactDOM.render(
    <MyComponent/>, document.getElementById('root')
