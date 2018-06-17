bb0a7e4776d10e9abdc920776b52782bb86bf484
V Saravanan <saravanan.resume@gmail.com>
2018-06-17 19:11:59 +0800

    state

--- src/index.js
+++ src/index.js
@@ -2,33 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import axios from 'axios';
 
- 
-class MyComponent extends React.Component{
-        render(){
-        return(   
-              <div>
-               <h1>Hello</h1>
-               <Header name="maxx" id="101"/>
-           </div>
-        );
-    }
-}
- 
-function Header(props) {
-    return (              
-           <div>
-            <Footer name = {props.name} id = {props.id}/>
-           </div>
-    );
-}
-function Footer(props) {
-    return (             
-           <div>             
-            <h1> Welcome : {props.name}</h1>             
-            <h1> Id is : {props.id}</h1>
-           </div>
-  );
-}
+class MyComponent extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          name: 'Maxx',
+          id: '101'
+      }
+  }
+  render()
+      {
+          setTimeout(()=>{this.setState({name:'Jaeha', id:'222'})},2000)
+          return (             
+                 <div>                     
+                   <h1>Hello {this.state.name}</h1>
+                   <h2>Your Id is {this.state.id}</h2>
+                 </div>
+           );
+      }
+  }
+
 ReactDOM.render(
    <MyComponent/>, document.getElementById('root')
 );
