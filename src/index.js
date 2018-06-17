56b6346dbda7547aea38958903f2cbb8417dfa76
V Saravanan <saravanan.resume@gmail.com>
2018-06-17 16:10:10 +0800

    POST

--- src/index.js
+++ src/index.js
@@ -2,24 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+
+
 export default class PersonList extends React.Component {
   state = {
-    persons: []
+    name: '',
   }
 
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      name: this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
       .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
+        console.log(res);
+        console.log(res.data);
       })
   }
 
   render() {
     return (
-      <ul>
-        { this.state.persons.map(person => <li>{person.name}</li>)}
-      </ul>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
     )
   }
 }
